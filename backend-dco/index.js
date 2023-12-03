56d157b06d0b3f7a585064301a00e301f4971ae3
Eduardo Castro <educastro2610@gmail.com>
2023-12-03 08:15:20 -0600

    V4.3

--- backend-dco/index.js
+++ backend-dco/index.js
@@ -1,40 +1,36 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-const conectarDB = require('./config/db');
+const express = require("express");
+var bodyParser = require("body-parser");
+const conectarDB = require("./config/db");
 const cors = require("cors");
-const path = require('path');
-const multipart = require('connect-multiparty');
+const path = require("path");
+const multipart = require("connect-multiparty");
 //Creamos el servidor
 const app = express();
 
 //Conectamos a la DB
 
-app.use(cors({origin: 'http://localhost:4200'}));
-conectarDB()
-app.set('trust proxy', 1);
-
+app.use(cors({ origin: "industria-app-industria.ashstg.easypanel.host" }));
+conectarDB();
+app.set("trust proxy", 1);
 
 //Middleware
 //Se usa para ejecutar funciones middleware
-app.use(bodyParser.json({ limit: '200mb' }));
-app.use(bodyParser.urlencoded({
-    limit: '200mb',
-    extended: true
-}
-));
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "200mb",
+    extended: true,
+  })
+);
 
 //rutas
 
-app.use('/api/usuarios',require('./routes/usuario'));
-app.use('/api/productos',require('./routes/producto'));
-app.use('/empresa/',require('./routes/empresa'));
-app.use('/admin/',require('./routes/admin'));
-app.use('/api/paginas',require('./routes/pagina'));
-
+app.use("/api/usuarios", require("./routes/usuario"));
+app.use("/api/productos", require("./routes/producto"));
+app.use("/empresa/", require("./routes/empresa"));
+app.use("/admin/", require("./routes/admin"));
+app.use("/api/paginas", require("./routes/pagina"));
 
 app.listen(3000, () => {
-    console.log('El servidor esta corriendo perfectamente');
+  console.log("El servidor esta corriendo perfectamente");
 });
-
-
-
